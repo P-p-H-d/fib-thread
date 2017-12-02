1573932bdc7c958ab29d03609fb6a413352175d5
Patrick Pelissier <patrick.pelissier@gmail.com>
2017-12-02 18:21:08 +0100

    Add new candidate.

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 BIN=fib-cilk
 CC=gcc -std=c99
 CFLAGS=-O2 -march=native -Wall
+WORKER_PATH_FLAGS=-I../mlib -I../m-worker ../m-worker/libmworker.a
 
-all: run-cilk run-openmp run-naive
+all: run-cilk run-openmp run-naive run-worker
 
 fib-cilk: fib-cilk.c
 	$(CC) $(CFLAGS) -fcilkplus fib-cilk.c -o fib-cilk
@@ -12,6 +13,9 @@ fib-openmp: fib-openmp.c
 
 fib-naive: fib-naive.c
 	$(CC) $(CFLAGS) fib-naive.c -o fib-naive -lpthread 
+
+fib-worker: fib-worker.c
+	$(CC) $(CFLAGS) fib-worker.c $(WORKER_PATH_FLAGS) -o fib-worker -lpthread
 
 run-cilk: fib-cilk
 	@echo Fibonacci by cilk+
@@ -25,5 +29,9 @@ run-naive: fib-naive
 	@echo Fibonacci by naive implementation
 	@perf stat ./fib-naive
 
+run-worker: fib-worker
+	@echo Fibonacci by m-worker implementation
+	@perf stat ./fib-worker
+
 clean:
-	rm -f fib-cilk fib-openmp fib-naive *~
+	rm -f fib-cilk fib-openmp fib-naive fib-worker *~
---
+++ fib-worker.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <time.h>
+#include <sys/time.h>
+#include <assert.h>
+
+#include "m-worker.h"
+
+static int
+cputime (void)
+{
+#if 0
+  /* Wrong for multi thread code since it measures the time spent 
+     by all threads not the real time */
+  struct rusage rus;
+
+  getrusage (0, &rus);
+  return rus.ru_utime.tv_sec * 1000 + rus.ru_utime.tv_usec / 1000;
+#else
+  struct timeval tv;
+  gettimeofday (&tv, NULL);
+  return tv.tv_sec * 1000 + tv.tv_usec / 1000;  
+#endif
+}
+
+/***************************************************************************/
+
+int fib(int n);
+
+struct fib2_s {
+  int x, n;
+};
+
+static void subfunc_1 (void *data) {
+  struct fib2_s *f = data;
+  f->x = fib (f->n );
+}
+
+/* Compute Fibonacci number using thread systems. */
+int fib(int n)
+{
+  if (n < 2)
+    return n;
+
+  struct fib2_s f;
+  worker_block_t b;
+
+  worker_start(b);
+  f.n = n - 2;
+  worker_spawn (b, subfunc_1, &f);
+  int y = fib (n-1);
+  worker_sync(b);
+  return f.x + y;
+}
+
+int main()
+{
+  worker_init(0, 0, NULL);
+  int n = 39;
+  int start = cputime();
+  int result = fib(n);
+  int end = cputime();
+
+  // Display our results
+  double duration = (double)(end - start) / 1000;
+  printf("Fibonacci number #%d is %d.\n", n, result);
+  printf("Calculated in %.3f seconds.\n", duration);
+
+  return 0;
+}
